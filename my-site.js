8309a4da1003c1929ba762fc157570b8befb12f2
far-code <157044309+Far-Coodinator@users.noreply.github.com>
2024-11-11 21:27:51 +0300

    Add files via upload

--- my-site.js
+++ my-site.js
@@ -1,3 +1,16 @@
+// . Disable Right-Click and Keyboard Shortcuts 
+  // document.addEventListener('contextmenu', event => event.preventDefault());
+  // document.addEventListener('keydown', function(event) {
+  //   if (event.key === 'F12' || 
+  //       (event.ctrlKey && (event.key === 'u' || event.key === 'U' || 
+  //                          event.key === 's' || event.key === 'S' || 
+  //                          event.key === 'I' || event.key === 'i'))) {
+  //     event.preventDefault();
+  //   }
+  // });
+
+
+
 // this script to open the nav-bar of mobile
 let openBtn = document.querySelector('#nav-mob-openBtn')
 let closeBtn = document.querySelector('#closebtn')
